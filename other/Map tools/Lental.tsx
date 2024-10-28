36414bb969427cbb521ceb81694ce8bd2ec778bb
Jaaslet <jonas_bms@hotmail.com>
2024-10-28 07:07:38 +0100

    Started the map

---
+++ other/Map tools/Lental.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Lental" tilewidth="16" tileheight="16" tilecount="6000" columns="100">
+ <image source="../../../../../Downloads/Lental.png" width="1600" height="960"/>
+</tileset>
